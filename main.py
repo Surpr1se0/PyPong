32f68f0a9ff492001145e022ab1066ff02c51644
surprise <franciscoggouveia@gmail.com>
2022-02-24 19:36:31 +0000

    Add Menu

--- main.py
+++ main.py
@@ -7,9 +7,93 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)  # stop the window updating
 
+# Menu
+
+menu = turtle.Turtle()
+menu.speed(0)
+menu.color("white")
+
+# Creating Menu option buttons
+def button(length):
+    for i in range(4):
+        menu.forward(length)
+        menu.left(90)
+
+
+def column(n, length):
+    menu.left(270)
+    for i in range(n):
+        button(length)
+        menu.forward(length)
+    menu.penup()
+    menu.left(90)
+    menu.forward(n * length)
+    menu.left(180)
+    menu.pendown()
+
+column(5, 100)
+
+def btnclick(x,y):
+    if 0 < x < 101 and 0 > y > -101:
+        print("Start Game")
+        print(x, y)
+        turtle.clearscreen()
+    elif 0 < x < 101 and -101 > y > -201:
+        print("Rules")
+        print(x, y)
+        turtle.clearscreen()
+    elif 0 < x < 101 and -201 > y > -301:
+        print("Highscore")
+        print(x, y)
+        turtle.clearscreen()
+    elif 0 < x < 101 and -301 > y > -401:
+        print("Hi")
+        print(x, y)
+        turtle.clearscreen()
+    elif 0 < x < 101 and -401 > y > -501:
+        print("Hi")
+        print(x, y)
+        turtle.clearscreen()
+    elif 0 < x < 101 and -501 > y > -601:
+        print("Hi")
+        print(x, y)
+        turtle.clearscreen()
+    else:
+        print("Click One Of The Options!")
+        print(x, y)
+        btnclick(x, y)
+turtle.onscreenclick(btnclick, 1)
+turtle.listen()
+
+turtle.done()
+
+
+# Menu Options
+menu.penup()
+menu.goto(8, -46)
+menu.write("START GAME!", font=("Arial", 12, "normal"))
+
+menu.penup()
+menu.goto(6, -145)
+menu.write("RULES", font=("Arial", 12, "normal"))
+
+menu.penup()
+menu.goto(3, -248)
+menu.write("HIGH SCORE", font=("Arial", 12, "normal"))
+
+menu.penup()
+menu.goto(4, -343)
+menu.write("FAQ", font=("Arial", 12, "normal"))
+
+menu.penup()
+menu.goto(3, -450)
+menu.write("QUIT GAME", font=("Arial", 12, "normal"))
+
+turtle.done()
+
 # Score
-score_a = 0;
-score_b = 0;
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -50,6 +134,7 @@ pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "norm
 
 
 # Funcs
+
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -107,24 +192,24 @@ while True:
         ball.dx *= -1
         score_a += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}" .format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
         score_b += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}" .format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle Bounce
-    if ball.xcor() > 340 and ball.xcor() < 350 and (
-            ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+    if 340 < ball.xcor() < 350 and (
+            paddle_b.ycor() + 50 > ball.ycor() > paddle_b.ycor() - 50):
         ball.setx(340)
         ball.dx *= -1
         os.system("aplay bounce.wav&")
 
-    if ball.xcor() < -340 and ball.xcor() > -350 and (
-            ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
+    if -340 > ball.xcor() > -350 and (
+            paddle_a.ycor() + 50 > ball.ycor() > paddle_a.ycor() - 50):
         ball.setx(-340)
         ball.dx *= -1
         os.system("aplay bounce.wav&")
