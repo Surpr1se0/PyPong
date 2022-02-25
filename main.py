30ec2cd1b5ffc7afe7d2d414c45553d51cc92e99
surprise <franciscoggouveia@gmail.com>
2022-02-25 16:53:37 +0000

    Add menu, not finished

--- main.py
+++ main.py
@@ -7,55 +7,33 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)  # stop the window updating
 
+
 # Menu
 
-text1 = turtle.Turtle()
-text1.speed(0)
-text1.pensize(2)
-text1.goto(0, 40)
-text1.color("white")
-text1.write("START", align="center", font=("Courier", 24, "normal"))
-text1.penup()
+def Text_Menu():
+    text1 = turtle.Turtle()
+    text1.speed(0)
+    text1.pensize(2)
+    text1.goto(0, 40)
+    text1.color("white")
+    text1.write("START", align="center", font=("Courier", 24, "normal"))
+    text1.penup()
 
-text2 = turtle.Turtle()
-text2.speed(0)
-text2.pensize(2)
-text2.goto(0, -20)
-text2.color("white")
-text2.write("FAQ", align="center", font=("Courier", 24, "normal"))
-text2.penup()
+    text2 = turtle.Turtle()
+    text2.speed(0)
+    text2.pensize(2)
+    text2.goto(0, -20)
+    text2.color("white")
+    text2.write("FAQ", align="center", font=("Courier", 24, "normal"))
+    text2.penup()
 
-
-text3 = turtle.Turtle()
-text3.speed(0)
-text3.pensize(2)
-text3.goto(0, -80)
-text3.color("white")
-text3.write("EXIT", align="center", font=("Courier", 24, "normal"))
-text3.penup()
-
-#Making options clickable
-def btnclick(x,y):
-    if -50 < x < 50 and 80 > y > -0:
-        print("START")
-        print(x, y)
-        turtle.clearscreen()
-    elif -50 < x < 50 and -0 > y > -50:
-        print("FAQ")
-        print(x, y)
-        turtle.clearscreen()
-    elif -50 < x < 50 and -50 > y > -100:
-        print("EXIT")
-        print(x, y)
-        turtle.clearscreen()
-    else:
-        print("Click One Of The Options!")
-        print(x, y)
-        btnclick(x, y)
-
-turtle.onscreenclick(btnclick, 1)
-turtle.listen()
-turtle.done()
+    text3 = turtle.Turtle()
+    text3.speed(0)
+    text3.pensize(2)
+    text3.goto(0, -80)
+    text3.color("white")
+    text3.write("EXIT", align="center", font=("Courier", 24, "normal"))
+    text3.penup()
 
 
 # Score
@@ -136,7 +114,10 @@ window.onkey(paddle_b_up, "Up")  # Listening for inputs Player 2
 window.onkey(paddle_b_down, "Down")
 
 # Main game loop
+
+
 while True:
+
     window.update()
 
     # Move the Ball
@@ -159,14 +140,16 @@ while True:
         ball.dx *= -1
         score_a += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
+                  font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
         score_b += 1
         pen.clear()
-        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
+                  font=("Courier", 24, "normal"))
 
     # Paddle Bounce
     if 340 < ball.xcor() < 350 and (
